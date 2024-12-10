dbbf37e7e943f4cf4819b22bd31da9a3d30624a4
Stefan Tasic <stefantasic@outlook.com>
2024-12-10 15:52:17 -0500

    client-server model first

--- snapdb.c
+++ snapdb.c
@@ -1,19 +1,3 @@
-int hashTableSize = 1000;
-
-typedef struct {
-
-} keyValue;
-
-unsigned int hash(char* key) {
-    unsigned int hashValue = 0;
-    while (*key) {
-        hashValue = hashValue << 5;
-        hashValue += *key;
-        *key++;
-    }
-    return hashValue % hashTableSize;
-}
-
 int main(int argc, char* argv) {
 
 }
