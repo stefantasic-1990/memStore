58c1521832ba165395cd53ba453e43915b1b87f5
Stefan Tasic <stefantasic@outlook.com>
2024-12-18 13:57:46 -0500

    begin work on server initialization and data structure

--- snapDB.c
+++ snapDB.c
@@ -1,8 +1,22 @@
 #include "snapDB.h"
 #include <stdio.h>
 
+unsigned long hash(const char *str) {
+    unsigned long hash = 5381;
+    int c;
+    while ((c = *str++)) {
+        hash = ((hash << 5) + hash) + c;
+    }
+
+    return hash;
+}
+
 int initializeDatabase() {
     printf("Initializing database...\n");
     printf("Database sucessfully initialized.\n");
     return 0;
 }
+
+int put();
+int remove();
+char* get();
--- snapDB.h
+++ snapDB.h
@@ -1,6 +1,14 @@
 #ifndef SNAPDB_H
 #define SNAPDB_H
 
+typedef struct {
+    char* key;
+    char* value;
+} KeyValue;
+
 int initializeDatabase();
+int put();
+int remove();
+char* get();
 
 #endif
